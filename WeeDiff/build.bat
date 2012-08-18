91a78fba62b9d5420975ce3297ce9215c8097bba
Ai4rei <ai4rei@users.noreply.github.com>
2012-08-18 18:53:40 +0000

    * Upgraded builder to release-build all projects.
    git-svn-id: svn://192.168.0.2/atwl/trunk@22 308214b9-756b-4e5d-a632-11ace8911769

---
+++ WeeDiff/build.bat
@@ -0,0 +1,55 @@
+@ECHO OFF
+
+:SETUP
+ECHO Setting up builder...
+REM Obtain current directory
+ECHO SET BUILDERDIR=^^>~~~build.bat
+CD>>~~~build.bat
+CALL ~~~build.bat
+DEL ~~~build.bat
+REM
+SET APPBUILDER=CL /nologo /c /W3 /O2x /GF /EHsc /DWIN32 /DNDEBUG /D_WINDOWS /D_UNICODE /DUNICODE %%i\*.cpp /I"%BUILDERDIR%" /I"%BUILDERDIR%\Snippets" /I"%BUILDERDIR%\WeeDiff\Common"
+SET DLLBUILDER=CL /nologo /W3 /O2x /FD /GF /EHsc /DWIN32 /DNDEBUG /D_WINDOWS /D_UNICODE /DUNICODE /I"%BUILDERDIR%\WeeDiff\Common" %%i\*.cpp /link /NOLOGO /MACHINE:IX86 /DLL /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /RELEASE kernel32.lib user32.lib gdi32.lib comdlg32.lib
+IF EXIST Release\NUL RD Release /s /q
+MD Release
+MD Release\plugins
+MD Release\plugins\WeeDiffGen
+
+:BUILD
+ECHO Building WeeDiff...
+FOR %%i IN (Snippets WeeDiff\WeeDiff) DO %APPBUILDER% > NUL
+IF ERRORLEVEL 1 GOTO ERROR
+ECHO #include "windows.h">afxres.h
+RC /FoWeeDiff.res /I. WeeDiff\WeeDiff\WeeDiff.rc > NUL
+IF ERRORLEVEL 1 GOTO ERROR
+LINK /NOLOGO /MACHINE:IX86 /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /RELEASE kernel32.lib user32.lib gdi32.lib comdlg32.lib *.obj WeeDiff.res /OUT:"%BUILDERDIR%\Release\WeeDiff.exe" > NUL
+IF ERRORLEVEL 1 GOTO ERROR
+
+ECHO Building WeeDiff plugins...
+CD WeeDiff
+FOR %%i IN (WeeDiffPlain WeeDiffGen) DO %DLLBUILDER% /OUT:"%BUILDERDIR%\Release\plugins\%%i.dll" > NUL
+DEL *.obj
+CD "%BUILDERDIR%"
+
+ECHO Building WeeDiffGen plugins...
+CD WeeDiff\WeeDiffGen\WeeDiffGen
+FOR %%i IN (WDGOnlyFirstLoginBackground WDGRemoveGravityLogo WDGSkipServiceSelect WDGEnableOfficialCustomFonts WDGEnableDNSSupport WDGDisable4LetterUserPasswordLimit WDGEnable127Hairstyles WDGAlwaysCallSelectKoreaClientInfo WDGFixCameraAnglesMedium WDGDisableMultipleWindows WDGUseRagnarokIcon WDGDisable1rag1 WDGChatAtBug WDGIncreaseZoomOut50Per WDGRestoreLoginWindow WDGReadQuestTable WDGAllowMultipleWindows WDGOnlySecondLoginBackground WDGExtendedChatRoomBox WDGEnforceOfficialLoginBackground WDGSkipLicenseScreen WDGIgnoreMissingFileErrors WDGDisableSwearFilter WDGFixCameraAnglesLess WDGReadMsgStringTable WDGDisableNagleAlgorithm WDGUseArialOnAllLangtypes WDGIncreaseHeadgearViewID WDGDisable4LetterUserIDLimit WDGDisable4LetterUserCharacterLimit WDGEnableMultipleGRFExtended WDGDisableHallucinationWavyScreen WDGIgnoreMissingPaletteErrors WDGSkipResurrectionButtons WDGEnableAuraOverLvl99 WDGRemoveGravityAds WDGIncreaseZoomOutMax WDGTranslateClientIntoEnglish WDGEnableMultipleGRF WDGEnableTitleBarMenu WDGAllowChatFlood WDGDisableFilenameCheck WDGCustomWindowTitle WDGDisableHShield WDGExtendedPMBox WDGLoadLUABeforeLUB WDGHKLMtoHKCU WDGExtendedChatBox WDGIncreaseZoomOut75Per WDGReadDataFolderFirst WDGUseNormalGuildBrackets WDGUseCustomAuraSprites WDGFixCameraAnglesFull WDGUsePlainTextDescriptions) DO %DLLBUILDER% /OUT:"%BUILDERDIR%\Release\plugins\WeeDiffGen\%%i.dll" > NUL)
+DEL *.obj
+CD "%BUILDERDIR%"
+
+:TEMP
+ECHO Removing temporary files...
+IF EXIST afxres.h DEL afxres.h
+DEL *.exp /s > NUL
+DEL *.lib /s > NUL
+DEL *.obj /s > NUL
+DEL *.idb /s > NUL
+SET DLLBUILDER=
+SET APPBUILDER=
+SET BUILDERDIR=
+GOTO EXIT
+
+:ERROR
+ECHO Error occured.
+
+:EXIT
