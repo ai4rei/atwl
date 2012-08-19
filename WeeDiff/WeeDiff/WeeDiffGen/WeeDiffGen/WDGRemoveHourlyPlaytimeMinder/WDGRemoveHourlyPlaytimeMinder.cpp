1d98c2807c9fdb431912cd01802ea9966ed598dc
Ai4rei <ai4rei@users.noreply.github.com>
2012-08-19 18:29:40 +0000

    + Added WDGRemoveHourlyPlaytimeMinder WeeDiffGen plugin.
    git-svn-id: svn://192.168.0.2/atwl/trunk@27 308214b9-756b-4e5d-a632-11ace8911769

---
+++ WeeDiff/WeeDiff/WeeDiffGen/WeeDiffGen/WDGRemoveHourlyPlaytimeMinder/WDGRemoveHourlyPlaytimeMinder.cpp
@@ -0,0 +1,218 @@
+// -----------------------------------------------------------------
+// WDGRemoveHourlyPlaytimeMinder
+// (c) 2012 Ai4rei/AN
+//
+// This work is licensed under a
+// Creative Commons BY-NC-SA 3.0 Unported License
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+// -----------------------------------------------------------------
+
+#include "WDGRemoveHourlyPlaytimeMinder.h"
+
+#ifndef _ARRAYSIZE
+    #define _ARRAYSIZE(x) (sizeof(x)/sizeof((x)[0]))
+#endif  /* _ARRAYSIZE */
+
+static /* const */ WDGPLUGININFO l_PluginInfo =
+{
+    _T("Remove Hourly Playtime-Minder"),
+    _T("Removes hourly announces keeping track of your time spent in-game and telling you to stop playing after 3 hours."),
+    _T("[UI]"),
+    _T(""),
+    _T("Ai4rei/AN"),
+    1,
+    0,
+    { 0x6161b1e1, 0xea19, 0x11e1, { 0xa7, 0x78, 0x0, 0x22, 0x15, 0x64, 0x8a, 0x98 } },  /* guid */
+    _T(""),
+};
+
+static WDGPlugin* l_lpSelfReference = NULL;
+
+void WDGPlugin::Release(void)
+{
+    this->m_DiffData.clear();
+    l_lpSelfReference = NULL;
+    delete this;
+}
+
+void WDGPlugin::Free(LPVOID lpBuf)
+{
+    delete lpBuf;
+}
+
+LPWDGPLUGININFO WDGPlugin::GetPluginInfo(void)
+{
+    return &l_PluginInfo;
+}
+
+INT32 WDGPlugin::Enabled(void)
+{
+    return 0;
+}
+
+INT32 WDGPlugin::Disabled(void)
+{
+    return 0;
+}
+
+LPCTSTR WDGPlugin::GetInputValue(void)
+{
+    return NULL;
+}
+
+DiffData* WDGPlugin::GeneratePatch(void)
+{
+    FINDDATA Fd;
+    UINT32 uOffset;
+
+    this->m_DiffData.clear();
+
+    if(!this->IsSane())
+    {
+        return NULL;
+    }
+
+    for(;;)
+    {// multiple choice
+    try
+    {
+        // MISSION: Find an unsigned integer division through
+        // 3600000 followed by JLE in CGameMode::PollNetworkStatus.
+
+        Fd.uMask = WFD_PATTERN|WFD_WILDCARD|WFD_SECTION;
+        Fd.chWildCard = '?';
+        Fd.lpszSection = ".text";
+
+        // 2009-06-17aSakexe ~ 2010-08-10aRagexeRE (VC6)
+        Fd.lpData =
+                  "8BC8"         // MOV     ECX,EAX
+                  "2BCA"         // SUB     ECX,EDX
+                  "B8 B17C2195"  // MOV     EAX,95217CB1h
+                  "F7E1"         // MUL     ECX
+                  "8BFA"         // MOV     EDI,EDX
+                  "C1EF 15"      // SHR     EDI,15h
+                  "85FF"         // TEST    EDI,EDI
+                  "897D C8"      // MOV     [LOCAL.14],EDI
+                  "0F8E"         // JLE     ADDR v
+                  ;
+        if(this->TryMatch(&Fd, &uOffset))
+        {
+            this->SetByte(uOffset+21, 0x90);  // JLE -> NOP
+            this->SetByte(uOffset+22, 0xE9);  // JLE -> JMP
+            break;
+        }
+
+        // 2010-08-18bRagexeRE ~ ? (VC9)
+        Fd.lpData =
+                  "B8 B17C2195"  // MOV     EAX,95217CB1h
+                  "F7E1"         // MUL     ECX
+                  "8BFA"         // MOV     EDI,EDX
+                  "C1EF 15"      // SHR     EDI,15h
+                  "3B'?'"        // CMP     EDI,R32  ; R32 is initialized to 0
+                  "0F8E"         // JLE     ADDR v
+                  ;
+        uOffset = this->m_dgc->Match(&Fd);
+
+        this->SetByte(uOffset+14, 0x90);  // JLE -> NOP
+        this->SetByte(uOffset+15, 0xE9);  // JLE -> JMP
+    }
+    catch(const char* lpszThrown)
+    {
+        char szErrMsg[1024];
+
+        wsprintfA(szErrMsg, __FILE__" :: %s", lpszThrown);
+        this->m_dgc->LogMsg(szErrMsg);
+    }
+    break;
+    }
+
+    return this->m_DiffData.empty() ? NULL : &this->m_DiffData;
+}
+
+DiffData* WDGPlugin::GetDiffData(void)
+{
+    return this->m_DiffData.empty() ? NULL : &this->m_DiffData;
+}
+
+bool WDGPlugin::IsVC9Image(void)
+{
+    try
+    {
+        this->m_dgc->FindFunction("_encode_pointer");  // msvcr90.dll
+    }
+    catch(const char* lpszThrown)
+    {
+        return false;
+
+        // unused
+        (void)lpszThrown;
+    }
+
+    return true;
+}
+
+bool WDGPlugin::IsSane(void)
+{// ensure that it actually is a valid RO client
+#define ISSANEMAGIC "gravity"
+    FINDDATA Fd;
+
+    Fd.uMask       = WFD_SECTION;
+    Fd.lpData      = ISSANEMAGIC;
+    Fd.uDataSize   = sizeof(ISSANEMAGIC);
+    Fd.lpszSection = this->IsVC9Image() ? ".rdata" : ".data";
+
+    try
+    {
+        this->m_dgc->Match(&Fd);
+    }
+    catch(const char* lpszThrown)
+    {
+        this->m_dgc->LogMsg(__FILE__" :: Image is not sane.");
+        return false;
+
+        // unused
+        (void)lpszThrown;
+    }
+
+    return true;
+#undef ISSANEMAGIC
+}
+
+void WDGPlugin::SetByte(UINT32 uOffset, UCHAR uValue)
+{
+    DIFFDATA Diff = { uOffset, uValue };
+
+    this->m_DiffData.push_back(Diff);
+}
+
+extern "C" __declspec(dllexport) WeeDiffGenPlugin::IWDGPlugin* InitPlugin(LPVOID lpData, USHORT huWeeDiffMajorVersion, USHORT huWeeDiffMinorVersion)
+{
+    if(l_lpSelfReference)
+    {// double initialization
+        DebugBreak();
+    }
+    else
+    {
+        l_lpSelfReference = new WDGPlugin(lpData);
+    }
+
+    return l_lpSelfReference;
+}
+
+bool WDGPlugin::TryMatch(LPFINDDATA lpFd, UINT32* lpuOffset)
+{
+    try
+    {
+        lpuOffset[0] = this->m_dgc->Match(lpFd);
+    }
+    catch(const char* lpszThrown)
+    {
+        return false;
+
+        // unused
+        (void)lpszThrown;
+    }
+
+    return true;
+}
---
+++ WeeDiff/WeeDiff/WeeDiffGen/WeeDiffGen/WDGRemoveHourlyPlaytimeMinder/WDGRemoveHourlyPlaytimeMinder.h
@@ -0,0 +1,45 @@
+// -----------------------------------------------------------------
+// WDGRemoveHourlyPlaytimeMinder
+// (c) 2012 Ai4rei/AN
+//
+// This work is licensed under a
+// Creative Commons BY-NC-SA 3.0 Unported License
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+// -----------------------------------------------------------------
+
+#ifndef _WDGREMOVEHOURLYPLAYTIMEMINDER_H_
+#define _WDGREMOVEHOURLYPLAYTIMEMINDER_H_
+
+#include <tchar.h>
+
+#include <WeeDiffGenPlugin.h>
+
+using namespace WeeDiffGenPlugin;
+
+class WDGPlugin : public IWDGPlugin
+{
+    DiffData m_DiffData;
+
+public:
+    WDGPlugin(LPVOID lpData) : IWDGPlugin(lpData)
+    {
+    }
+    //
+    virtual void Release(void);
+    virtual void Free(LPVOID lpBuf);
+    virtual LPWDGPLUGININFO GetPluginInfo(void);
+    virtual INT32 Enabled(void);
+    virtual INT32 Disabled(void);
+    virtual LPCTSTR GetInputValue(void);
+    virtual DiffData* GeneratePatch(void);
+    virtual DiffData* GetDiffData(void);
+
+private:
+    bool IsVC9Image(void);
+    bool IsSane(void);
+    void SetByte(UINT32 uOffset, UCHAR uValue);
+    bool TryMatch(LPFINDDATA lpFd, UINT32* lpuOffset);
+};
+
+#endif  /* _WDGREMOVEHOURLYPLAYTIMEMINDER_H_ */
