4c3ea3442a0510b333d059123dc91f6520cd9d51
Ai4rei <ai4rei@users.noreply.github.com>
2012-08-19 16:59:44 +0000

    + Added WDGEnableAsciiInText WeeDiffGen plugin.
    git-svn-id: svn://192.168.0.2/atwl/trunk@25 308214b9-756b-4e5d-a632-11ace8911769

---
+++ WeeDiff/WeeDiff/WeeDiffGen/WeeDiffGen/WDGEnableAsciiInText/WDGEnableAsciiInText.cpp
@@ -0,0 +1,183 @@
+// -----------------------------------------------------------------
+// WDGEnableAsciiInText
+// (c) 2012 Ai4rei/AN
+//
+// This work is licensed under a
+// Creative Commons BY-NC-SA 3.0 Unported License
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+// -----------------------------------------------------------------
+
+#include "WDGEnableAsciiInText.h"
+
+#ifndef _ARRAYSIZE
+    #define _ARRAYSIZE(x) (sizeof(x)/sizeof((x)[0]))
+#endif  /* _ARRAYSIZE */
+
+static /* const */ WDGPLUGININFO l_PluginInfo =
+{
+    _T("Enable ASCII in Text"),
+    _T("Allows use of non-english characters in chat."),
+    _T("[UI]"),
+    _T(""),
+    _T("Ai4rei/AN"),
+    1,
+    0,
+    { 0x6161b1e0, 0xea19, 0x11e1, { 0xa7, 0x78, 0x0, 0x22, 0x15, 0x64, 0x8a, 0x98 } },  /* guid */
+    _T("Recommended"),
+};
+
+static WDGPlugin* l_lpSelfReference = NULL;
+
+void WDGPlugin::Release(void)
+{
+    this->m_DiffData.clear();
+    l_lpSelfReference = NULL;
+    delete this;
+}
+
+void WDGPlugin::Free(LPVOID lpBuf)
+{
+    delete lpBuf;
+}
+
+LPWDGPLUGININFO WDGPlugin::GetPluginInfo(void)
+{
+    return &l_PluginInfo;
+}
+
+INT32 WDGPlugin::Enabled(void)
+{
+    return 0;
+}
+
+INT32 WDGPlugin::Disabled(void)
+{
+    return 0;
+}
+
+LPCTSTR WDGPlugin::GetInputValue(void)
+{
+    return NULL;
+}
+
+DiffData* WDGPlugin::GeneratePatch(void)
+{
+    FINDDATA Fd;
+    UINT32 uOffset;
+
+    this->m_DiffData.clear();
+
+    if(!this->IsSane())
+    {
+        return NULL;
+    }
+
+    try
+    {
+        // MISSION: Find TEST BYTE PTR DS:[R32+R32],80 followed by a
+        // JNZ SHORT in CSession::IsOnlyEnglish.
+
+        // NOTE: actually, this misses arrays addressed with ESP+R32
+        //       and EBP+EBP, but let's hope the compiler will never
+        //       use these as they require an additional prefix.
+        Fd.uMask = WFD_PATTERN|WFD_WILDCARD|WFD_SECTION;
+        Fd.lpData =
+                  "F604'?' 80"  // TEST    BYTE PTR DS:[R32+R32],80
+                  "75 '?'"      // JNZ     SHORT ADDR v
+                  "'?'"         // INC     R32
+                  "3B'?'"       // CMP     R32,R32
+                  "7C F5"       // JL      SHORT ADDR ^
+                  ;
+        Fd.chWildCard = '?';
+        Fd.lpszSection = ".text";
+
+        uOffset = this->m_dgc->Match(&Fd);
+
+        // while neutering the entire function would be more
+        // effective it's more compatible to disable the JNZ that
+        // causes the function to fail. also the footprint is
+        // smaller this way ( 2 NOP's v. RETN 4 ).
+        this->SetByte(uOffset+4, 0x90);
+        this->SetByte(uOffset+5, 0x90);
+    }
+    catch(const char* lpszThrown)
+    {
+        char szErrMsg[1024];
+
+        wsprintfA(szErrMsg, __FILE__" :: %s", lpszThrown);
+        this->m_dgc->LogMsg(szErrMsg);
+    }
+
+    return this->m_DiffData.empty() ? NULL : &this->m_DiffData;
+}
+
+DiffData* WDGPlugin::GetDiffData(void)
+{
+    return this->m_DiffData.empty() ? NULL : &this->m_DiffData;
+}
+
+bool WDGPlugin::IsVC9Image(void)
+{
+    try
+    {
+        this->m_dgc->FindFunction("_encode_pointer");  // msvcr90.dll
+    }
+    catch(const char* lpszThrown)
+    {
+        return false;
+
+        // unused
+        (void)lpszThrown;
+    }
+
+    return true;
+}
+
+bool WDGPlugin::IsSane(void)
+{// ensure that it actually is a valid RO client
+#define ISSANEMAGIC "gravity"
+    FINDDATA Fd;
+
+    Fd.uMask       = WFD_SECTION;
+    Fd.lpData      = ISSANEMAGIC;
+    Fd.uDataSize   = sizeof(ISSANEMAGIC);
+    Fd.lpszSection = this->IsVC9Image() ? ".rdata" : ".data";
+
+    try
+    {
+        this->m_dgc->Match(&Fd);
+    }
+    catch(const char* lpszThrown)
+    {
+        this->m_dgc->LogMsg(__FILE__" :: Image is not sane.");
+        return false;
+
+        // unused
+        (void)lpszThrown;
+    }
+
+    return true;
+#undef ISSANEMAGIC
+}
+
+void WDGPlugin::SetByte(UINT32 uOffset, UCHAR uValue)
+{
+    DIFFDATA Diff = { uOffset, uValue };
+
+    this->m_DiffData.push_back(Diff);
+}
+
+extern "C" __declspec(dllexport) WeeDiffGenPlugin::IWDGPlugin* InitPlugin(LPVOID lpData, USHORT huWeeDiffMajorVersion, USHORT huWeeDiffMinorVersion)
+{
+    if(l_lpSelfReference)
+    {// double initialization
+        DebugBreak();
+    }
+    else
+    {
+        l_lpSelfReference = new WDGPlugin(lpData);
+    }
+
+    return l_lpSelfReference;
+}
---
+++ WeeDiff/WeeDiff/WeeDiffGen/WeeDiffGen/WDGEnableAsciiInText/WDGEnableAsciiInText.h
@@ -0,0 +1,44 @@
+// -----------------------------------------------------------------
+// WDGEnableAsciiInText
+// (c) 2012 Ai4rei/AN
+//
+// This work is licensed under a
+// Creative Commons BY-NC-SA 3.0 Unported License
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+// -----------------------------------------------------------------
+
+#ifndef _WDGENABLEASCIIINTEXT_H_
+#define _WDGENABLEASCIIINTEXT_H_
+
+#include <tchar.h>
+
+#include <WeeDiffGenPlugin.h>
+
+using namespace WeeDiffGenPlugin;
+
+class WDGPlugin : public IWDGPlugin
+{
+    DiffData m_DiffData;
+
+public:
+    WDGPlugin(LPVOID lpData) : IWDGPlugin(lpData)
+    {
+    }
+    //
+    virtual void Release(void);
+    virtual void Free(LPVOID lpBuf);
+    virtual LPWDGPLUGININFO GetPluginInfo(void);
+    virtual INT32 Enabled(void);
+    virtual INT32 Disabled(void);
+    virtual LPCTSTR GetInputValue(void);
+    virtual DiffData* GeneratePatch(void);
+    virtual DiffData* GetDiffData(void);
+
+private:
+    bool IsVC9Image(void);
+    bool IsSane(void);
+    void SetByte(UINT32 uOffset, UCHAR uValue);
+};
+
+#endif  /* _WDGENABLEASCIIINTEXT_H_ */
