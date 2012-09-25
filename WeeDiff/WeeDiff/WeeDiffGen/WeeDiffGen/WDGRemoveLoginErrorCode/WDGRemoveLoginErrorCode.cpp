f32801b49aad91022c6b4f9942a59a8ce18fda8d
Ai4rei <ai4rei@users.noreply.github.com>
2012-09-25 17:34:06 +0000

    + Added WeeDiffGen plugin WDGRemoveLoginErrorCode.
    git-svn-id: svn://192.168.0.2/atwl/trunk@69 308214b9-756b-4e5d-a632-11ace8911769

---
+++ WeeDiff/WeeDiff/WeeDiffGen/WeeDiffGen/WDGRemoveLoginErrorCode/WDGRemoveLoginErrorCode.cpp
@@ -0,0 +1,172 @@
+// -----------------------------------------------------------------
+// WDGRemoveLoginErrorCode
+// (c) 2012 Ai4rei/AN
+//
+// This work is licensed under a
+// Creative Commons BY-NC-SA 3.0 Unported License
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+// -----------------------------------------------------------------
+
+#include "WDGRemoveLoginErrorCode.h"
+
+#ifndef _ARRAYSIZE
+    #define _ARRAYSIZE(x) (sizeof(x)/sizeof((x)[0]))
+#endif  /* _ARRAYSIZE */
+
+static /* const */ WDGPLUGININFO l_PluginInfo =
+{
+    _T("Remove Login Error Code"),
+    _T("Removes numeric error code in parentheses from login error messages."),
+    _T("[UI]"),
+    _T(""),
+    _T("Ai4rei/AN"),
+    1,
+    0,
+    { 0x6161b1ea, 0xea19, 0x11e1, { 0xa7, 0x78, 0x0, 0x22, 0x15, 0x64, 0x8a, 0x98 } },  /* guid */
+    _T(""),
+};
+
+static WDGPlugin* l_lpSelfReference = NULL;
+
+void WDGPlugin::Release(void)
+{
+    this->m_DiffData.clear();
+    l_lpSelfReference = NULL;
+    delete this;
+}
+
+void WDGPlugin::Free(LPVOID lpBuf)
+{
+    delete lpBuf;
+}
+
+LPWDGPLUGININFO WDGPlugin::GetPluginInfo(void)
+{
+    return &l_PluginInfo;
+}
+
+INT32 WDGPlugin::Enabled(void)
+{
+    return 0;
+}
+
+INT32 WDGPlugin::Disabled(void)
+{
+    return 0;
+}
+
+LPCTSTR WDGPlugin::GetInputValue(void)
+{
+    return this->m_szBufferSize;
+}
+
+DiffData* WDGPlugin::GeneratePatch(void)
+{
+    FINDDATA Fd;
+    UINT32 uOffset, uPart;
+
+    this->m_DiffData.clear();
+
+    if(!this->IsSane())
+    {
+        return NULL;
+    }
+
+    try
+    {
+        // MISSION: Find the format code, and cut the error code
+        // part out.
+
+        // Find
+        Fd.uMask = WFD_PATTERN|WFD_SECTION;
+        Fd.lpData = "'%s(%d)' 00 00";
+        Fd.lpszSection = ".rdata";
+
+        uPart = 1;
+
+        uOffset = this->m_dgc->Match(&Fd);
+
+        // Cut it out (really)
+        this->SetByte(uOffset+2, 0x00);
+    }
+    catch(const char* lpszThrown)
+    {
+        char szErrMsg[1024];
+
+        wsprintfA(szErrMsg, __FILE__" :: Part %u :: %s", uPart, lpszThrown);
+        this->m_dgc->LogMsg(szErrMsg);
+    }
+
+    return this->m_DiffData.empty() ? NULL : &this->m_DiffData;
+}
+
+DiffData* WDGPlugin::GetDiffData(void)
+{
+    return this->m_DiffData.empty() ? NULL : &this->m_DiffData;
+}
+
+bool WDGPlugin::IsVC9Image(void)
+{
+    try
+    {
+        this->m_dgc->FindFunction("_encode_pointer");  // msvcr90.dll
+    }
+    catch(const char* lpszThrown)
+    {
+        return false;
+
+        // unused
+        (void)lpszThrown;
+    }
+
+    return true;
+}
+
+bool WDGPlugin::IsSane(void)
+{// ensure that it actually is a valid RO client
+#define ISSANEMAGIC "gravity"
+    FINDDATA Fd;
+
+    Fd.uMask       = WFD_SECTION;
+    Fd.lpData      = ISSANEMAGIC;
+    Fd.uDataSize   = sizeof(ISSANEMAGIC);
+    Fd.lpszSection = this->IsVC9Image() ? ".rdata" : ".data";
+
+    try
+    {
+        this->m_dgc->Match(&Fd);
+    }
+    catch(const char* lpszThrown)
+    {
+        this->m_dgc->LogMsg(__FILE__" :: Image is not sane.");
+        return false;
+
+        // unused
+        (void)lpszThrown;
+    }
+
+    return true;
+#undef ISSANEMAGIC
+}
+
+void WDGPlugin::SetByte(UINT32 uOffset, UCHAR uValue)
+{
+    DIFFDATA Diff = { uOffset, uValue };
+
+    this->m_DiffData.push_back(Diff);
+}
+
+extern "C" __declspec(dllexport) WeeDiffGenPlugin::IWDGPlugin* InitPlugin(LPVOID lpData, USHORT huWeeDiffMajorVersion, USHORT huWeeDiffMinorVersion)
+{
+    if(l_lpSelfReference)
+    {// double initialization
+        DebugBreak();
+    }
+    else
+    {
+        l_lpSelfReference = new WDGPlugin(lpData);
+    }
+
+    return l_lpSelfReference;
+}
