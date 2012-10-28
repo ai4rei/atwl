4de29d058b6a95f5cc1ce1e46bec2b30f88d2b57
Ai4rei <ai4rei@users.noreply.github.com>
2012-10-28 15:08:28 +0000

    + Added WeeDiffGen plugin WDGChatColorNormal.
    git-svn-id: svn://192.168.0.2/atwl/trunk@82 308214b9-756b-4e5d-a632-11ace8911769

---
+++ WeeDiff/WeeDiff/WeeDiffGen/WeeDiffGen/WDGChatColorNormal/WDGChatColorNormal.h
@@ -0,0 +1,48 @@
+// -----------------------------------------------------------------
+// WDGChatColorNormal
+// (c) 2012 Ai4rei/AN
+//
+// This work is licensed under a
+// Creative Commons BY-NC-SA 3.0 Unported License
+// http://creativecommons.org/licenses/by-nc-sa/3.0/
+//
+// -----------------------------------------------------------------
+
+#ifndef _WDGCHATCOLORNORMAL_H_
+#define _WDGCHATCOLORNORMAL_H_
+
+#include <tchar.h>
+
+#include <WeeDiffGenPlugin.h>
+
+using namespace WeeDiffGenPlugin;
+
+class WDGPlugin : public IWDGPlugin
+{
+    DiffData m_DiffData;
+    COLORREF m_crChatColor;
+    TCHAR m_szColor[8];
+
+public:
+    WDGPlugin(LPVOID lpData) : IWDGPlugin(lpData)
+    {
+        this->m_crChatColor = RGB(0xff,0xff,0xff);
+        this->m_szColor[0] = 0;
+    }
+    //
+    virtual void Release(void);
+    virtual void Free(LPVOID lpBuf);
+    virtual LPWDGPLUGININFO GetPluginInfo(void);
+    virtual INT32 Enabled(void);
+    virtual INT32 Disabled(void);
+    virtual LPCTSTR GetInputValue(void);
+    virtual DiffData* GeneratePatch(void);
+    virtual DiffData* GetDiffData(void);
+
+private:
+    bool IsVC9Image(void);
+    bool IsSane(void);
+    void SetByte(UINT32 uOffset, UCHAR uValue);
+};
+
+#endif  /* _WDGCHATCOLORNORMAL_H_ */
