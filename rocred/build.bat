dd91a977c48d70e5db75443182caf85c28719cde
Ai4rei <ai4rei@users.noreply.github.com>
2013-05-31 18:40:09 +0000

    git-svn-id: svn://192.168.0.2/atwl/trunk@136 308214b9-756b-4e5d-a632-11ace8911769

--- rocred/build.bat
+++ rocred/build.bat
@@ -1,9 +1,9 @@
+@ECHO OFF
 REM ----------------------------------------------------------------
 REM RO Credentials (ROCred)
 REM (c) 2012-2013 Ai4rei/AN
 REM
 REM ----------------------------------------------------------------
 
-@ECHO OFF
 RC /Forocred.res rocred.rc
 CL /W3 /O1s /GF /GA /I"C:\Progra~1\Micros~1\SDK\include" /I"..\snippets" rocred.c ..\snippets\dlgabout.c ..\snippets\dlgtempl.c ..\snippets\md5.c ..\snippets\xf_binhex.c rocred.res user32.lib gdi32.lib comctl32.lib /link /OPT:REF /OPT:ICF /OPT:NOWIN98 /RELEASE
