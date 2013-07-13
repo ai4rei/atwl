6f6cfb0cfd4283d99cf02c7041202e72f1f3615b
Ai4rei <ai4rei@users.noreply.github.com>
2013-07-13 09:34:49 +0000

    + Added cleaning mechanism.
    git-svn-id: svn://192.168.0.2/atwl/trunk@147 308214b9-756b-4e5d-a632-11ace8911769

--- rocred/build.bat
+++ rocred/build.bat
@@ -5,5 +5,12 @@ REM (c) 2012-2013 Ai4rei/AN
 REM
 REM ----------------------------------------------------------------
 
+IF "%1"=="clean" GOTO CLEAN
+:BUILD
 RC /Forocred.res rocred.rc
 CL /nologo /W3 /O1s /GF /GA /I"C:\Progra~1\Micros~1\SDK\include" /I"..\snippets" rocred.c ..\snippets\dlgabout.c ..\snippets\dlgtempl.c ..\snippets\md5.c ..\snippets\xf_binhex.c rocred.res user32.lib gdi32.lib comctl32.lib shell32.lib /link /OPT:REF /OPT:ICF /OPT:NOWIN98 /RELEASE
+GOTO END
+:CLEAN
+FOR %%v IN (*.obj rocred.res) DO IF EXIST %%v DEL %%v
+GOTO END
+:END
