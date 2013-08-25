d8a6325fbda6b65242d9d9570b8644e04324c6be
Ai4rei <ai4rei@users.noreply.github.com>
2013-08-25 09:00:29 +0000

    * Whitespace for the sake of code style.
    git-svn-id: svn://192.168.0.2/atwl/trunk@173 308214b9-756b-4e5d-a632-11ace8911769

--- rocred/rocred.c
+++ rocred/rocred.c
@@ -183,69 +183,69 @@ static BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     switch(uMsg)
     {
         case WM_INITDIALOG:
+        {
+            char szBuffer[4096];
+            BOOL bCheckSave, bSetFocus = TRUE;
+            HINSTANCE hInstance = GetModuleHandleA(NULL);
+
+            BgSkinOnCreate(hWnd);
+
+            SendMessage(hWnd, WM_SETICON, ICON_BIG,
+                (LPARAM)LoadImage(hInstance, MAKEINTRESOURCE(1), IMAGE_ICON, 32, 32, LR_SHARED));
+            SendMessage(hWnd, WM_SETICON, ICON_SMALL,
+                (LPARAM)LoadImage(hInstance, MAKEINTRESOURCE(2), IMAGE_ICON, 16, 16, LR_SHARED));
+
+            SetWindowTextA(hWnd, l_szAppTitle);
+
+            LoadStringA(hInstance, IDS_USERNAME, szBuffer, __ARRAYSIZE(szBuffer));
+            SetWindowTextA(GetDlgItem(hWnd, IDS_USERNAME), szBuffer);
+
+            LoadStringA(hInstance, IDS_PASSWORD, szBuffer, __ARRAYSIZE(szBuffer));
+            SetWindowTextA(GetDlgItem(hWnd, IDS_PASSWORD), szBuffer);
+
+            if(ConfigGetInt("PolicyNoCheckSave"))
             {
-                char szBuffer[4096];
-                BOOL bCheckSave, bSetFocus = TRUE;
-                HINSTANCE hInstance = GetModuleHandleA(NULL);
+                ShowWindow(GetDlgItem(hWnd, IDC_CHECKSAVE), SW_HIDE);
+            }
+            else
+            {
+                LoadStringA(hInstance, IDS_CHECKSAVE, szBuffer, __ARRAYSIZE(szBuffer));
+                SetWindowTextA(GetDlgItem(hWnd, IDC_CHECKSAVE), szBuffer);
 
-                BgSkinOnCreate(hWnd);
+                bCheckSave = ConfigGetInt("CheckSave");
+                SendMessage(GetDlgItem(hWnd, IDC_CHECKSAVE), BM_SETCHECK, (WPARAM)bCheckSave, 0);
 
-                SendMessage(hWnd, WM_SETICON, ICON_BIG,
-                    (LPARAM)LoadImage(hInstance, MAKEINTRESOURCE(1), IMAGE_ICON, 32, 32, LR_SHARED));
-                SendMessage(hWnd, WM_SETICON, ICON_SMALL,
-                    (LPARAM)LoadImage(hInstance, MAKEINTRESOURCE(2), IMAGE_ICON, 16, 16, LR_SHARED));
-
-                SetWindowTextA(hWnd, l_szAppTitle);
-
-                LoadStringA(hInstance, IDS_USERNAME, szBuffer, __ARRAYSIZE(szBuffer));
-                SetWindowTextA(GetDlgItem(hWnd, IDS_USERNAME), szBuffer);
-
-                LoadStringA(hInstance, IDS_PASSWORD, szBuffer, __ARRAYSIZE(szBuffer));
-                SetWindowTextA(GetDlgItem(hWnd, IDS_PASSWORD), szBuffer);
-
-                if(ConfigGetInt("PolicyNoCheckSave"))
+                if(bCheckSave)
                 {
-                    ShowWindow(GetDlgItem(hWnd, IDC_CHECKSAVE), SW_HIDE);
-                }
-                else
-                {
-                    LoadStringA(hInstance, IDS_CHECKSAVE, szBuffer, __ARRAYSIZE(szBuffer));
-                    SetWindowTextA(GetDlgItem(hWnd, IDC_CHECKSAVE), szBuffer);
+                    ConfigGetStr("UserName", szBuffer, __ARRAYSIZE(szBuffer));
 
-                    bCheckSave = ConfigGetInt("CheckSave");
-                    SendMessage(GetDlgItem(hWnd, IDC_CHECKSAVE), BM_SETCHECK, (WPARAM)bCheckSave, 0);
-
-                    if(bCheckSave)
+                    if(szBuffer[0])
                     {
-                        ConfigGetStr("UserName", szBuffer, __ARRAYSIZE(szBuffer));
-
-                        if(szBuffer[0])
-                        {
-                            SetWindowTextA(GetDlgItem(hWnd, IDC_USERNAME), szBuffer);
-                            SetFocus(GetDlgItem(hWnd, IDC_PASSWORD));  // move focus to password
-                            bSetFocus = FALSE;
-                        }
+                        SetWindowTextA(GetDlgItem(hWnd, IDC_USERNAME), szBuffer);
+                        SetFocus(GetDlgItem(hWnd, IDC_PASSWORD));  // move focus to password
+                        bSetFocus = FALSE;
                     }
                 }
-
-                LoadStringA(hInstance, IDS_OK, szBuffer, __ARRAYSIZE(szBuffer));
-                SetWindowTextA(GetDlgItem(hWnd, IDOK), szBuffer);
-
-                LoadStringA(hInstance, IDS_CLOSE, szBuffer, __ARRAYSIZE(szBuffer));
-                SetWindowTextA(GetDlgItem(hWnd, IDCANCEL), szBuffer);
-
-                if(ConfigGetInt("PolicyNoReplay"))
-                {
-                    ShowWindow(GetDlgItem(hWnd, IDB_REPLAY), SW_HIDE);
-                }
-                else
-                {
-                    LoadStringA(hInstance, IDS_REPLAY, szBuffer, __ARRAYSIZE(szBuffer));
-                    SetWindowTextA(GetDlgItem(hWnd, IDB_REPLAY), szBuffer);
-                }
-
-                return bSetFocus;
             }
+
+            LoadStringA(hInstance, IDS_OK, szBuffer, __ARRAYSIZE(szBuffer));
+            SetWindowTextA(GetDlgItem(hWnd, IDOK), szBuffer);
+
+            LoadStringA(hInstance, IDS_CLOSE, szBuffer, __ARRAYSIZE(szBuffer));
+            SetWindowTextA(GetDlgItem(hWnd, IDCANCEL), szBuffer);
+
+            if(ConfigGetInt("PolicyNoReplay"))
+            {
+                ShowWindow(GetDlgItem(hWnd, IDB_REPLAY), SW_HIDE);
+            }
+            else
+            {
+                LoadStringA(hInstance, IDS_REPLAY, szBuffer, __ARRAYSIZE(szBuffer));
+                SetWindowTextA(GetDlgItem(hWnd, IDB_REPLAY), szBuffer);
+            }
+
+            return bSetFocus;
+        }
         case WM_COMMAND:
             if(HIWORD(wParam)!=1  && HIWORD(wParam)!=0)
             {
@@ -254,90 +254,92 @@ static BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             else switch(LOWORD(wParam))
             {
                 case IDB_REPLAY:
-                    {
-                        char szExePath[MAX_PATH];
-                        char szExeName[MAX_PATH];
-                        char szExeType[16];
+                {
+                    char szExePath[MAX_PATH];
+                    char szExeName[MAX_PATH];
+                    char szExeType[16];
 
-                        ConfigGetStr("ExeName", szExeName, __ARRAYSIZE(szExeName));
-                        ConfigGetStr("ExeType", szExeType, __ARRAYSIZE(szExeType));
-                        CombineExePathName(szExePath, __ARRAYSIZE(szExePath), szExeName);
+                    ConfigGetStr("ExeName", szExeName, __ARRAYSIZE(szExeName));
+                    ConfigGetStr("ExeType", szExeType, __ARRAYSIZE(szExeType));
+                    CombineExePathName(szExePath, __ARRAYSIZE(szExePath), szExeName);
 
-                        // Replay mode
-                        InvokeProcess(hWnd, szExePath, "-t:Replay %s", szExeType);
-                    }
+                    // Replay mode
+                    InvokeProcess(hWnd, szExePath, "-t:Replay %s", szExeType);
+
                     //EndDialog(hWnd, 1);
                     break;
+                }
                 case IDOK:
+                {
+                    char szUserName[24];
+                    char szPassWord[24];
+                    char szExePath[MAX_PATH];
+                    char szExeName[MAX_PATH];
+                    char szExeType[16];
+                    char szBuffer[4096];
+                    BOOL bCheckSave;
+                    HINSTANCE hInstance = GetModuleHandleA(NULL);
+
+                    GetWindowTextA(GetDlgItem(hWnd, IDC_USERNAME), szUserName, __ARRAYSIZE(szUserName));
+
+                    if(lstrlenA(szUserName)<4)
                     {
-                        char szUserName[24];
-                        char szPassWord[24];
-                        char szExePath[MAX_PATH];
-                        char szExeName[MAX_PATH];
-                        char szExeType[16];
-                        char szBuffer[4096];
-                        BOOL bCheckSave;
-                        HINSTANCE hInstance = GetModuleHandleA(NULL);
-
-                        GetWindowTextA(GetDlgItem(hWnd, IDC_USERNAME), szUserName, __ARRAYSIZE(szUserName));
-
-                        if(lstrlenA(szUserName)<4)
-                        {
-                            LoadStringA(hInstance, szUserName[0] ? IDS_USER_SHRT : IDS_USER_NONE, szBuffer, __ARRAYSIZE(szBuffer));
-                            MessageBox(hWnd, szBuffer, l_szAppTitle, MB_OK|MB_ICONINFORMATION);
-                            break;
-                        }
-
-                        GetWindowTextA(GetDlgItem(hWnd, IDC_PASSWORD), szPassWord, __ARRAYSIZE(szPassWord));
-
-                        if(lstrlenA(szPassWord)<4)
-                        {
-                            LoadStringA(hInstance, szPassWord[0] ? IDS_PASS_SHRT : IDS_PASS_NONE, szBuffer, __ARRAYSIZE(szBuffer));
-                            MessageBox(hWnd, szBuffer, l_szAppTitle, MB_OK|MB_ICONINFORMATION);
-                            break;
-                        }
-
-                        if(ConfigGetInt("PolicyNoSessionPassword"))
-                        {
-                            SetWindowTextA(GetDlgItem(hWnd, IDC_PASSWORD), "");
-                        }
-
-                        bCheckSave = (BOOL)(SendMessage(GetDlgItem(hWnd, 103), BM_GETCHECK, 0, 0)==BST_CHECKED);
-                        ConfigSetStr("CheckSave", bCheckSave ? "1" : "0");
-
-                        if(bCheckSave)
-                        {// save
-                            ConfigSetStr("UserName", szUserName);
-                        }
-                        else
-                        {// delete
-                            ConfigSetStr("UserName", NULL);
-                        }
-
-                        ConfigGetStr("ExeName", szExeName, __ARRAYSIZE(szExeName));
-                        ConfigGetStr("ExeType", szExeType, __ARRAYSIZE(szExeType));
-                        CombineExePathName(szExePath, __ARRAYSIZE(szExePath), szExeName);
-
-                        if(ConfigGetInt("HashMD5"))
-                        {// MD5
-                            uint8 ucHash[4*4];
-                            char szHexHash[4*4*2+1];
-
-                            MD5_String(szPassWord, ucHash);
-                            XF_BinHex(szHexHash, __ARRAYSIZE(szHexHash), ucHash, __ARRAYSIZE(ucHash));
-
-                            InvokeProcess(hWnd, szExePath, "-t:%s %s %s", szHexHash, szUserName, szExeType);
-                        }
-                        else
-                        {// Plaintext
-                            InvokeProcess(hWnd, szExePath, "-t:%s %s %s", szPassWord, szUserName, szExeType);
-                        }
-
-                        // get rid of the password after it has been used
-                        ZeroMemory((void*)(volatile void*)szPassWord, sizeof(szPassWord));
+                        LoadStringA(hInstance, szUserName[0] ? IDS_USER_SHRT : IDS_USER_NONE, szBuffer, __ARRAYSIZE(szBuffer));
+                        MessageBox(hWnd, szBuffer, l_szAppTitle, MB_OK|MB_ICONINFORMATION);
+                        break;
                     }
+
+                    GetWindowTextA(GetDlgItem(hWnd, IDC_PASSWORD), szPassWord, __ARRAYSIZE(szPassWord));
+
+                    if(lstrlenA(szPassWord)<4)
+                    {
+                        LoadStringA(hInstance, szPassWord[0] ? IDS_PASS_SHRT : IDS_PASS_NONE, szBuffer, __ARRAYSIZE(szBuffer));
+                        MessageBox(hWnd, szBuffer, l_szAppTitle, MB_OK|MB_ICONINFORMATION);
+                        break;
+                    }
+
+                    if(ConfigGetInt("PolicyNoSessionPassword"))
+                    {
+                        SetWindowTextA(GetDlgItem(hWnd, IDC_PASSWORD), "");
+                    }
+
+                    bCheckSave = (BOOL)(SendMessage(GetDlgItem(hWnd, 103), BM_GETCHECK, 0, 0)==BST_CHECKED);
+                    ConfigSetStr("CheckSave", bCheckSave ? "1" : "0");
+
+                    if(bCheckSave)
+                    {// save
+                        ConfigSetStr("UserName", szUserName);
+                    }
+                    else
+                    {// delete
+                        ConfigSetStr("UserName", NULL);
+                    }
+
+                    ConfigGetStr("ExeName", szExeName, __ARRAYSIZE(szExeName));
+                    ConfigGetStr("ExeType", szExeType, __ARRAYSIZE(szExeType));
+                    CombineExePathName(szExePath, __ARRAYSIZE(szExePath), szExeName);
+
+                    if(ConfigGetInt("HashMD5"))
+                    {// MD5
+                        uint8 ucHash[4*4];
+                        char szHexHash[4*4*2+1];
+
+                        MD5_String(szPassWord, ucHash);
+                        XF_BinHex(szHexHash, __ARRAYSIZE(szHexHash), ucHash, __ARRAYSIZE(ucHash));
+
+                        InvokeProcess(hWnd, szExePath, "-t:%s %s %s", szHexHash, szUserName, szExeType);
+                    }
+                    else
+                    {// Plaintext
+                        InvokeProcess(hWnd, szExePath, "-t:%s %s %s", szPassWord, szUserName, szExeType);
+                    }
+
+                    // get rid of the password after it has been used
+                    ZeroMemory((void*)(volatile void*)szPassWord, sizeof(szPassWord));
+
                     //EndDialog(hWnd, 1);
                     break;
+                }
                 case IDCANCEL:
                     EndDialog(hWnd, 0);
                     break;
@@ -358,22 +360,22 @@ static BOOL CALLBACK DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         case WM_CTLCOLORSTATIC:
             return BgSkinOnCtlColorStatic((HDC)wParam);
         case WM_HELP:
+        {
+            DLGABOUTINFO Dai =
             {
-                DLGABOUTINFO Dai =
-                {
-                    hWnd,
-                    MAKELONG(2012,2013),
-                    "About ROCred...",
-                    "ROCred",
-                    APP_VERSION,
-                    "Ai4rei/AN",
-                    "\r\nThis software is FREEWARE and is provided AS IS, without warranty of ANY KIND, either expressed or implied, including but not limited to the implied warranties of merchantability and/or fitness for a particular purpose. If your country's law does not allow complete exclusion of liability, you may not use this software. The author SHALL NOT be held liable for ANY damage to you, your hardware, your software, your pets, your dear other, or to anyone or anything else, that may or may not result from the use or misuse of this software. Basically, you use it at YOUR OWN RISK.",
-                    "http://hercules.ws/board/index.php?showtopic=1466",
-                };
+                hWnd,
+                MAKELONG(2012,2013),
+                "About ROCred...",
+                "ROCred",
+                APP_VERSION,
+                "Ai4rei/AN",
+                "\r\nThis software is FREEWARE and is provided AS IS, without warranty of ANY KIND, either expressed or implied, including but not limited to the implied warranties of merchantability and/or fitness for a particular purpose. If your country's law does not allow complete exclusion of liability, you may not use this software. The author SHALL NOT be held liable for ANY damage to you, your hardware, your software, your pets, your dear other, or to anyone or anything else, that may or may not result from the use or misuse of this software. Basically, you use it at YOUR OWN RISK.",
+                "http://hercules.ws/board/index.php?showtopic=1466",
+            };
 
-                DlgAbout(&Dai);
-            }
+            DlgAbout(&Dai);
             break;
+        }
         default:
             return FALSE;
     }
