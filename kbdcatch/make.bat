c2bbe4bf9f5eb1ff23f3e7d6b41e2280164a662f
Ai4rei <ai4rei@users.noreply.github.com>
2013-12-24 13:16:53 +0000

    git-svn-id: svn://192.168.0.2/atwl/trunk@220 308214b9-756b-4e5d-a632-11ace8911769

--- kbdcatch/make.bat
+++ kbdcatch/make.bat
@@ -3,7 +3,9 @@ IF EXIST *.obj DEL *.obj
 IF EXIST kbdcatch.res DEL kbdcatch.res
 IF EXIST kbdcatch.sys DEL kbdcatch.sys
 IF "%1"=="clean" GOTO END
+SET CLOPT=/O2x /GF /DNDEBUG
+IF "%1"=="debug" SET CLOPT=/Od /DDBG /DDEBUG /D_DEBUG
 RC /Fokbdcatch.res kbdcatch.rc
-CL /nologo /c /W3 /IC:\NTDDK\inc /IC:\NTDDK\inc\ddk /Od /Gz /DWIN32 /D_WIN32 /D_X86_ kbdcatch.c kbdcdevs.c kbdcodev.c
+CL /nologo /c /W3 %CLOPT% /Gz /DWIN32 /D_WIN32 /D_X86_ /IC:\NTDDK\inc /IC:\NTDDK\inc\ddk kbdcatch.c kbdcdevs.c kbdcodev.c
 LINK /NOLOGO /ENTRY:DriverEntry@8 /MACHINE:IX86 /NODEFAULTLIB /RELEASE /SUBSYSTEM:NATIVE /OUT:kbdcatch.sys *.obj kbdcatch.res C:\NTDDK\libfre\i386\ntoskrnl.lib C:\NTDDK\libfre\i386\hal.lib
 :END
