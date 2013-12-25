3f8612fa911c30950a31e8818ca1118273d94a98
Ai4rei <ai4rei@users.noreply.github.com>
2013-12-25 14:49:41 +0000

    * Adjusted the compile to generate a WDM driver, rather than a legacy one.
    git-svn-id: svn://192.168.0.2/atwl/trunk@224 308214b9-756b-4e5d-a632-11ace8911769

--- kbdcatch/make.bat
+++ kbdcatch/make.bat
@@ -4,8 +4,22 @@ IF EXIST kbdcatch.res DEL kbdcatch.res
 IF EXIST kbdcatch.sys DEL kbdcatch.sys
 IF "%1"=="clean" GOTO END
 SET CLOPT=/O2x /GF /DNDEBUG
+SET LNOPT=/OPT:REF /OPT:ICF C:\NTDDK\libfre\i386\wdm.lib
 IF "%1"=="debug" SET CLOPT=/Od /DDBG /DDEBUG /D_DEBUG
+IF "%1"=="debug" SET LNOPT=/OPT:NOREF /OPT:NOICF C:\NTDDK\libchk\i386\wdm.lib
 RC /Fokbdcatch.res kbdcatch.rc
 CL /nologo /c /W3 %CLOPT% /Gz /DWIN32 /D_WIN32 /D_X86_ /IC:\NTDDK\inc /IC:\NTDDK\inc\ddk kbdcatch.c kbdcdevs.c kbdcodev.c
-LINK /NOLOGO /ENTRY:DriverEntry@8 /MACHINE:IX86 /NODEFAULTLIB /RELEASE /SUBSYSTEM:NATIVE /OUT:kbdcatch.sys *.obj kbdcatch.res C:\NTDDK\libfre\i386\ntoskrnl.lib C:\NTDDK\libfre\i386\hal.lib
+LINK /NOLOGO %LNOPT% /SECTION:INIT,D /ENTRY:DriverEntry@8 /MACHINE:IX86 /NODEFAULTLIB /RELEASE /SUBSYSTEM:NATIVE,1.0 /OUT:kbdcatch.sys *.obj kbdcatch.res
+REM /DRIVER:WDM
+REM - Sets DllFlags to 0x2000 (WDM Driver)
+REM - Sets Subsystem to Native.
+REM - Unconditionally creates a new INIT section, that is RWEDC,
+REM   which causes a warning due to a clash with alloc_text and
+REM   makes the INIT section writable, which is not what we desire.
+REM
+REM So we do not use the switch, but set the subsystem and section
+REM ourselves and then mark the driver as a WDM one with ModPE.
+MODPE /NOLOGO /RELEASE /DLLFLAGS +WDM kbdcatch.sys
 :END
+SET CLOPT=
+SET LNOPT=
