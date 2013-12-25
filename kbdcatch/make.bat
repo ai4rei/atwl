18e83ab162272ec0db2833b4fdc5f8d13f416e16
Ai4rei <ai4rei@users.noreply.github.com>
2013-12-25 18:08:09 +0000

    git-svn-id: svn://192.168.0.2/atwl/trunk@225 308214b9-756b-4e5d-a632-11ace8911769

--- kbdcatch/make.bat
+++ kbdcatch/make.bat
@@ -9,17 +9,19 @@ IF "%1"=="debug" SET CLOPT=/Od /DDBG /DDEBUG /D_DEBUG
 IF "%1"=="debug" SET LNOPT=/OPT:NOREF /OPT:NOICF C:\NTDDK\libchk\i386\wdm.lib
 RC /Fokbdcatch.res kbdcatch.rc
 CL /nologo /c /W3 %CLOPT% /Gz /DWIN32 /D_WIN32 /D_X86_ /IC:\NTDDK\inc /IC:\NTDDK\inc\ddk kbdcatch.c kbdcdevs.c kbdcodev.c
-LINK /NOLOGO %LNOPT% /SECTION:INIT,D /ENTRY:DriverEntry@8 /MACHINE:IX86 /NODEFAULTLIB /RELEASE /SUBSYSTEM:NATIVE,1.0 /OUT:kbdcatch.sys *.obj kbdcatch.res
+LINK /NOLOGO %LNOPT% /ENTRY:DriverEntry@8 /MACHINE:IX86 /NODEFAULTLIB /RELEASE /SUBSYSTEM:NATIVE,1.0 /OUT:kbdcatch.sys *.obj kbdcatch.res
 REM /DRIVER:WDM
 REM - Sets DllFlags to 0x2000 (WDM Driver)
 REM - Sets Subsystem to Native.
 REM - Unconditionally creates a new INIT section, that is RWEDC,
 REM   which causes a warning due to a clash with alloc_text and
 REM   makes the INIT section writable, which is not what we desire.
+REM - Updates all non-discardable sections to be non-pagable, except
+REM   PAGE*, .edata, .debug, .arch, .xdata, .sdata and .textbbs.
 REM
-REM So we do not use the switch, but set the subsystem and section
-REM ourselves and then mark the driver as a WDM one with ModPE.
-MODPE /NOLOGO /RELEASE /DLLFLAGS +WDM kbdcatch.sys
+REM So we do not use the switch, but set the subsystem ourselves and
+REM then modify the sections and mark the driver as WDM with ModPE.
+MODPE /NOLOGO /VERBOSE /RELEASE /DLLFLAGS +WDM /SECTIONFLAGS +INIT/D+.text/P+.rdata/P+.data/P kbdcatch.sys
 :END
 SET CLOPT=
 SET LNOPT=
