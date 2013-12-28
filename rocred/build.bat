3bf82844b67ae08f1fde466f22661e6fd25f472d
Ai4rei <ai4rei@users.noreply.github.com>
2013-12-28 10:44:21 +0000

    * Templated manifest and versioninfo.
    git-svn-id: svn://192.168.0.2/atwl/trunk@254 308214b9-756b-4e5d-a632-11ace8911769

--- rocred/build.bat
+++ rocred/build.bat
@@ -13,7 +13,7 @@ SET CLOPT=/nologo /c /W3 /O1s /GF /GA /IC:\Progra~1\Micros~1\SDK\include /I..\sn
 FOR %%v IN (bvector.c cstr.c dlgabout.c dlgtempl.c kvdb.c kvdb\win32ini.c macaddr.c md5.c memory.c regionui.c w32uxt.c xf_binhex.c xf_slash.c) DO CL %CLOPT% ..\snippets\%%v
 LIB /NOLOGO /OUT:snippets.lib *.obj
 DEL *.obj > NUL
-RC /Forocred.res rocred.rc
+RC /Forocred.res /I..\snippets rocred.rc
 CL %CLOPT% rocred.c bgskin.c button.c config.c
 LINK /NOLOGO /OPT:REF /OPT:ICF /OPT:NOWIN98 /RELEASE /OUT:rocred.exe *.obj rocred.res snippets.lib kernel32.lib user32.lib gdi32.lib comctl32.lib shell32.lib
 SET CLOPT=
