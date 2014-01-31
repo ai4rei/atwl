f2b864e07b9c67c89c9347a0b3aec2f99392a517
Ai4rei <ai4rei@users.noreply.github.com>
2014-01-31 18:27:07 +0000

    ! Fixed up build script.
    git-svn-id: svn://192.168.0.2/atwl/trunk@289 308214b9-756b-4e5d-a632-11ace8911769

--- WeeDiff/build.bat
+++ WeeDiff/build.bat
@@ -13,10 +13,14 @@ CD>>~~~build.bat
 CALL ~~~build.bat
 DEL ~~~build.bat
 REM
-SET APPBUILDER=CL /nologo /c /W4 /O2x /GF /EHsc /DWIN32 /DNDEBUG /D_WINDOWS /D_UNICODE /DUNICODE /DSTRICT %%i\*.cpp /I"%BUILDERDIR%" /I"%BUILDERDIR%\Snippets" /I"%BUILDERDIR%\WeeDiff\Common"
-SET DLLBUILDER=CL /nologo /W4 /O2x /FD /GF /EHsc /DWIN32 /DNDEBUG /D_WINDOWS /D_UNICODE /DUNICODE /DSTRICT /I"%BUILDERDIR%\WeeDiff\Common" %%i\*.cpp /link /NOLOGO /MACHINE:IX86 /DLL /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /RELEASE kernel32.lib user32.lib gdi32.lib comdlg32.lib
+SET APPBUILDER=CL /nologo /c /W4 /O2x /GF /EHsc /DWIN32 /DNDEBUG /D_WINDOWS /D_UNICODE /DUNICODE /DSTRICT %%i\*.cpp /I"%BUILDERDIR%" /I"%BUILDERDIR%\Snippets" /I"%BUILDERDIR%\Common"
+SET DLLBUILDER=CL /nologo /W4 /O2x /FD /GF /EHsc /DWIN32 /DNDEBUG /D_WINDOWS /D_UNICODE /DUNICODE /DSTRICT /I"%BUILDERDIR%" /I"%BUILDERDIR%\Common" %%i\*.cpp /link /NOLOGO /MACHINE:IX86 /DLL /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /RELEASE kernel32.lib user32.lib gdi32.lib comdlg32.lib
 IF EXIST Release\NUL RD Release /s /q
+SET OUT=NUL
+IF "%1"=="verbose" GOTO SETVERBOSE
+:DONEVERBOSE
 IF "%1"=="clean" GOTO TEMP
+REM Setup folders
 MD Release
 MD Release\plugins
 MD Release\plugins\WeeDiffGen
@@ -24,25 +28,23 @@ IF NOT "%1"=="" GOTO WDG
 
 :BUILD
 ECHO Building WeeDiff...
-FOR %%i IN (Snippets WeeDiff\WeeDiff) DO %APPBUILDER% > NUL
+FOR %%i IN (Snippets WeeDiff) DO %APPBUILDER% > %OUT%
 IF ERRORLEVEL 1 GOTO ERROR
 ECHO #include "windows.h">afxres.h
-RC /FoWeeDiff.res /I. WeeDiff\WeeDiff\WeeDiff.rc > NUL
+RC /FoWeeDiff.res /I. WeeDiff\WeeDiff.rc > %OUT%
 IF ERRORLEVEL 1 GOTO ERROR
-LINK /NOLOGO /MACHINE:IX86 /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /RELEASE kernel32.lib user32.lib gdi32.lib comdlg32.lib *.obj WeeDiff.res /OUT:"%BUILDERDIR%\Release\WeeDiff.exe" > NUL
+LINK /NOLOGO /MACHINE:IX86 /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /RELEASE kernel32.lib user32.lib gdi32.lib comdlg32.lib *.obj WeeDiff.res /OUT:"%BUILDERDIR%\Release\WeeDiff.exe" > %OUT%
 IF ERRORLEVEL 1 GOTO ERROR
 
 ECHO Building WeeDiff plugins...
-CD WeeDiff
-FOR %%i IN (WeeDiffPlain WeeDiffGen) DO %DLLBUILDER% /OUT:"%BUILDERDIR%\Release\plugins\%%i.dll" > NUL
-CD "%BUILDERDIR%"
+FOR %%i IN (WeeDiffPlain WeeDiffGen) DO %DLLBUILDER% /OUT:"%BUILDERDIR%\Release\plugins\%%i.dll" > %OUT%
 
 :WDG
 ECHO Building WeeDiffGen plugins...
-CD WeeDiff\WeeDiffGen\WeeDiffGen
+CD WeeDiffGen\Plugins
 IF NOT "%1"=="" GOTO WDGINDIVL
 REM Original
-FOR %%i IN (WDGOnlyFirstLoginBackground WDGRemoveGravityLogo WDGSkipServiceSelect WDGEnableOfficialCustomFonts WDGEnableDNSSupport WDGDisable4LetterUserPasswordLimit WDGEnable127Hairstyles WDGAlwaysCallSelectKoreaClientInfo WDGFixCameraAnglesMedium WDGDisableMultipleWindows WDGUseRagnarokIcon WDGDisable1rag1 WDGChatAtBug WDGIncreaseZoomOut50Per WDGReadQuestTable WDGAllowMultipleWindows WDGOnlySecondLoginBackground WDGExtendedChatRoomBox WDGEnforceOfficialLoginBackground WDGSkipLicenseScreen WDGDisableSwearFilter WDGFixCameraAnglesLess WDGReadMsgStringTable WDGDisableNagleAlgorithm WDGIncreaseHeadgearViewID WDGDisable4LetterUserIDLimit WDGDisable4LetterUserCharacterLimit WDGEnableMultipleGRFExtended WDGDisableHallucinationWavyScreen WDGIgnoreMissingPaletteErrors WDGSkipResurrectionButtons WDGEnableAuraOverLvl99 WDGRemoveGravityAds WDGIncreaseZoomOutMax WDGEnableTitleBarMenu WDGAllowChatFlood WDGDisableFilenameCheck WDGCustomWindowTitle WDGDisableHShield WDGExtendedPMBox WDGLoadLUABeforeLUB WDGHKLMtoHKCU WDGExtendedChatBox WDGIncreaseZoomOut75Per WDGReadDataFolderFirst WDGUseNormalGuildBrackets WDGUseCustomAuraSprites WDGFixCameraAnglesFull WDGUsePlainTextDescriptions) DO %DLLBUILDER% /OUT:"%BUILDERDIR%\Release\plugins\WeeDiffGen\%%i.dll" > NUL
+FOR %%i IN (WDGOnlyFirstLoginBackground WDGRemoveGravityLogo WDGSkipServiceSelect WDGEnableOfficialCustomFonts WDGEnableDNSSupport WDGDisable4LetterUserPasswordLimit WDGEnable127Hairstyles WDGAlwaysCallSelectKoreaClientInfo WDGFixCameraAnglesMedium WDGDisableMultipleWindows WDGUseRagnarokIcon WDGDisable1rag1 WDGChatAtBug WDGIncreaseZoomOut50Per WDGReadQuestTable WDGAllowMultipleWindows WDGOnlySecondLoginBackground WDGExtendedChatRoomBox WDGEnforceOfficialLoginBackground WDGSkipLicenseScreen WDGDisableSwearFilter WDGFixCameraAnglesLess WDGReadMsgStringTable WDGDisableNagleAlgorithm WDGIncreaseHeadgearViewID WDGDisable4LetterUserIDLimit WDGDisable4LetterUserCharacterLimit WDGEnableMultipleGRFExtended WDGDisableHallucinationWavyScreen WDGIgnoreMissingPaletteErrors WDGSkipResurrectionButtons WDGEnableAuraOverLvl99 WDGRemoveGravityAds WDGIncreaseZoomOutMax WDGEnableTitleBarMenu WDGAllowChatFlood WDGDisableFilenameCheck WDGCustomWindowTitle WDGDisableHShield WDGExtendedPMBox WDGLoadLUABeforeLUB WDGHKLMtoHKCU WDGExtendedChatBox WDGIncreaseZoomOut75Per WDGReadDataFolderFirst WDGUseNormalGuildBrackets WDGUseCustomAuraSprites WDGFixCameraAnglesFull WDGUsePlainTextDescriptions) DO %DLLBUILDER% /OUT:"%BUILDERDIR%\Release\plugins\WeeDiffGen\%%i.dll" > %OUT%
 REM Atwl
 FOR %%i IN (WDGEnableAsciiInText WDGRemoveHourlyPlaytimeMinder WDGRemoveHourlyGameGrade WDGRemoveQuakeSkillEffect WDGSharedBodyPalettes WDGSkipPacketHeaderObfuscation WDGSharedHeadPalettes WDGEnable64kHairstyles WDGUseSSOLoginPacket WDGExtendedNPCDialog WDGRemoveLoginErrorCode WDGTranslateClient WDGChatColorGM WDGChatColorNormal WDGChatColorSelf WDGChatColorGuild WDGChatColorParty WDGUseCustomFont WDGScreenshotQuality WDGEnableProxySupport WDGEnableCustom3DBones) DO %DLLBUILDER% /OUT:"%BUILDERDIR%\Release\plugins\WeeDiffGen\%%i.dll"
 GOTO WDGBUILT
@@ -54,17 +56,25 @@ CD "%BUILDERDIR%"
 :TEMP
 ECHO Removing temporary files...
 IF EXIST afxres.h DEL afxres.h
+IF EXIST weediff.res DEL weediff.res
+IF EXIST *.obj DEL *.obj
 CD Release
-DEL *.exp /s > NUL
-DEL *.lib /s > NUL
-CD ..\WeeDiff\WeeDiffGen\WeeDiffGen
-DEL *.obj > NUL
-DEL *.idb > NUL
+DEL *.exp /s > %OUT%
+DEL *.lib /s > %OUT%
+CD ..\WeeDiffGen\Plugins
+DEL *.obj > %OUT%
+DEL *.idb > %OUT%
 CD "%BUILDERDIR%"
 SET DLLBUILDER=
 SET APPBUILDER=
 SET BUILDERDIR=
+SET OUT=
 GOTO EXIT
+
+:SETVERBOSE
+SET OUT=CON
+SHIFT
+GOTO DONEVERBOSE
 
 :ERROR
 ECHO Error occured.
