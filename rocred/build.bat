2bea055977216622350f639a4bffdaa746c094be
Ai4rei <ai4rei@users.noreply.github.com>
2015-12-04 16:22:09 +0000

    * Refresh:   * Enabled ASLR and DEP.   - Removed some unused modules.
    git-svn-id: svn://192.168.0.2/atwl/trunk@342 308214b9-756b-4e5d-a632-11ace8911769

--- rocred/build.bat
+++ rocred/build.bat
@@ -9,13 +9,14 @@ REM ----------------------------------------------------------------
 FOR %%v IN (*.obj snippets.lib rocred.res) DO IF EXIST %%v DEL %%v
 IF "%1"=="clean" GOTO END
 :BUILD
-SET CLOPT=/nologo /c /W3 /O1s /GF /GA /IC:\Progra~1\Micros~1\SDK\include /I..\snippets
-FOR %%v IN (bvector.c bvcstr.cpp bvfont.cpp bvpars.cpp dlgabout.c dlgtempl.c kvdb.c kvdb\win32ini.c macaddr.c md5.c memory.c procbth.c regionui.c w32ui.cpp w32uxt.c wnttools.c xf_binhex.c xf_slash.c) DO CL %CLOPT% ..\snippets\%%v
+SET CLOPT=/nologo /c /W3 /O2x /GF /GA /Gy /IC:\Progra~1\Micros~1\SDK\include /I..\snippets
+FOR %%v IN (bvector dlgabout dlgtempl kvdb kvdb\win32ini macaddr md5 memory regionui w32uxt wnttools xf_slash) DO CL %CLOPT% ..\snippets\%%v.c
+FOR %%v IN (bvcstr bvfont bvpars w32ui xf_binhex) DO CL %CLOPT% ..\snippets\%%v.cpp
 LIB /NOLOGO /OUT:snippets.lib *.obj
 DEL *.obj > NUL
 RC /Forocred.res /I..\snippets rocred.rc
 CL %CLOPT% rocred.c bgskin.c button.c config.c
-LINK /NOLOGO /OPT:REF /OPT:ICF /OPT:NOWIN98 /RELEASE /DEBUG /OUT:rocred.exe *.obj rocred.res snippets.lib kernel32.lib user32.lib gdi32.lib comctl32.lib shell32.lib advapi32.lib ole32.lib
-MODPE /NOLOGO /RELEASE /DLLFLAGS +NXCOMPAT rocred.exe
+LINK /NOLOGO /OPT:REF /OPT:ICF /RELEASE /DEBUG /LARGEADDRESSAWARE /FIXED:NO /VERSION:1.9 /OUT:rocred.exe *.obj rocred.res snippets.lib kernel32.lib user32.lib gdi32.lib comctl32.lib shell32.lib advapi32.lib ole32.lib
+MODPE /NOLOGO /RELEASE /DLLFLAGS +DYNAMICBASE+NXCOMPAT+TSAWARE rocred.exe
 SET CLOPT=
 :END
