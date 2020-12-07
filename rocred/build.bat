923ef1ba0913eea096670ab9dc336eae745183c5
Ai4rei <ai4rei@users.noreply.github.com>
2020-12-07 04:52:50 +0000

    * No longer uses intermediate .lib. * Object files now go to obj\ * Reflected recent snippets changes.
    git-svn-id: svn://192.168.0.2/atwl/trunk@447 308214b9-756b-4e5d-a632-11ace8911769

--- rocred/build.bat
+++ rocred/build.bat
@@ -6,17 +6,16 @@ REM
 REM ----------------------------------------------------------------
 
 :CLEAN
-FOR %%v IN (*.obj snippets.lib rocred.res) DO IF EXIST %%v DEL %%v
+FOR %%v IN (obj\*.obj obj\rocred.res) DO IF EXIST %%v DEL %%v
+IF NOT EXIST obj\NUL MD obj
 IF "%1"=="clean" GOTO END
 :BUILD
-SET CLOPT=/nologo /c /W3 /O2x /GF /GA /DBVLLST_CHECK_UNBOUND_INSERT /Zi /I..\snippets
-FOR %%v IN (bvllst dlgabout dlgtempl kvdb _kvdb\win32ini macaddr md5 mem _mem\mem.win32.heap regionui rsrcio w32uxt wnttools xf_slash) DO CL %CLOPT% ..\snippets\%%v.c
-FOR %%v IN (bvcstr bvfont bvpars bvwide w32cred w32ex w32ui xf_binhex) DO CL %CLOPT% ..\snippets\%%v.cpp
-LIB /NOLOGO /OUT:snippets.lib *.obj
-DEL *.obj > NUL
-RC /Forocred.res /I..\snippets rocred.rc
+SET CLOPT=/nologo /c /W3 /O2x /Foobj\ /GF /GA /DBVLLST_CHECK_UNBOUND_INSERT /Zi /I..\snippets
+FOR %%v IN (bvllst dlgabout kvdb _kvdb\win32ini macaddr md5 mem _mem\mem.win32.heap regionui rsrcio w32uxt wnttools xf_slash) DO CL %CLOPT% ..\snippets\%%v.c
+FOR %%v IN (bvcstr bvfont bvpars bvwide dlgtempl w32cred w32ex w32gdi w32ui xf_binhex) DO CL %CLOPT% ..\snippets\%%v.cpp
+RC /Foobj\rocred.res /I..\snippets rocred.rc
 CL %CLOPT% rocred.c bgskin.c button.c config.c
-LINK /NOLOGO /OPT:REF /OPT:ICF /RELEASE /DEBUG /LARGEADDRESSAWARE /FIXED:NO /VERSION:1.9 /OUT:rocred.exe *.obj rocred.res snippets.lib kernel32.lib user32.lib gdi32.lib comctl32.lib shell32.lib advapi32.lib ole32.lib
+LINK /NOLOGO /OPT:REF /OPT:ICF /RELEASE /DEBUG /LARGEADDRESSAWARE /FIXED:NO /VERSION:1.9 /OUT:rocred.exe obj\*.obj obj\rocred.res kernel32.lib user32.lib gdi32.lib comctl32.lib shell32.lib advapi32.lib ole32.lib
 MODPE /NOLOGO /RELEASE /DLLFLAGS +DYNAMICBASE+NXCOMPAT+TSAWARE rocred.exe
 SET CLOPT=
 :END
