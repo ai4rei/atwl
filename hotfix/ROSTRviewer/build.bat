c996dd5ae560c025edb3c73cc06d0dc298e208f5
Ai4rei <ai4rei@users.noreply.github.com>
2021-01-20 19:08:00 +0000

    + Added version info.
    git-svn-id: svn://192.168.0.2/atwl/trunk@473 308214b9-756b-4e5d-a632-11ace8911769

--- hotfix/ROSTRviewer/build.bat
+++ hotfix/ROSTRviewer/build.bat
@@ -1,1 +1,3 @@
-@CL -nologo -W3 -LD -O2x -D_CRT_SECURE_NO_WARNINGS hotfix32.c -link -def:hotfix32.def
+@ECHO OFF
+RC -Fohotfix32.res hotfix32.rc
+CL -nologo -W3 -LD -O2x -GF -D_CRT_SECURE_NO_WARNINGS hotfix32.c hotfix32.res -link -def:hotfix32.def
