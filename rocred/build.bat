2033ae5664dfbccd7c85b7cbfec87166ad04fe66
Ai4rei <ai4rei@users.noreply.github.com>
2021-03-03 21:17:04 +0000

    * Re-use build templates.
    git-svn-id: svn://192.168.0.2/atwl/trunk@478 308214b9-756b-4e5d-a632-11ace8911769

--- rocred/build.bat
+++ rocred/build.bat
@@ -11,7 +11,7 @@ IF NOT EXIST obj\NUL MD obj
 IF "%1"=="clean" GOTO END
 :BUILD
 RC /Foobj\rocred.res /I..\snippets rocred.rc
-CL @rocred.build
+CL @..\snippets\build-release-default.build @rocred.build
 IF ERRORLEVEL 1 GOTO END
 MODPE /NOLOGO /RELEASE /DLLFLAGS +DYNAMICBASE+NXCOMPAT+TSAWARE rocred.exe
 :END
